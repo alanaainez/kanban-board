8015e9a173a21e71d665cde210ab322609981e8a
alanaainez <alainez649@gmail.com>
2025-02-26 17:52:36 -0500

    git commit -m "Env updated and authorizing"

--- Develop/server/src/middleware/auth.ts
+++ Develop/server/src/middleware/auth.ts
@@ -7,4 +7,11 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const token = req.headers['authorization']?.split(' ')[1];
+  if (!token) return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.body.user = user;
+    next();
+  }
 };
--- Develop/server/src/routes/auth-routes.ts
+++ Develop/server/src/routes/auth-routes.ts
@@ -5,6 +5,15 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+  const user = await User.findOne({
+    username,
+  });
+  if (!user) return res.sendStatus(401);
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) return res.sendStatus(401);
+  const token = jwt.sign({ username: user.username }, process.env.ACCESS_TOKEN_SECRET as string);
+  res.json({ token });
 };
 
 const router = Router();
