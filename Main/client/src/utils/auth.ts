a7b361a511808ea1d02d2ee872fb92f74431ba0a
alanaainez <alainez649@gmail.com>
2025-02-27 21:36:25 -0500

    git commit -m "Conducted npm i"

---
+++ Main/client/src/utils/auth.ts
@@ -0,0 +1,39 @@
+import { JwtPayload, jwtDecode } from 'jwt-decode';
+
+class AuthService {
+  getProfile() {
+    // TODO: return the decoded token
+    return jwtDecode<JwtPayload>(this.getToken());
+  }
+
+  loggedIn() {
+    // TODO: return a value that indicates if the user is logged in
+    return !!this.getToken();
+  }
+  
+  isTokenExpired(token: string) {
+    // TODO: return a value that indicates if the token is expired
+    const decoded = jwtDecode<JwtPayload>(token);
+    return decoded.exp < Date.now() / 1000;
+  }
+
+  getToken(): string {
+    // TODO: return the token
+    return localStorage.getItem('id_token') || '';
+  }
+
+  login(idToken: string) {
+    // TODO: set the token to localStorage
+    localStorage.setItem('id_token', idToken);
+    // TODO: redirect to the home page
+  }
+
+  logout() {
+    // TODO: remove the token from localStorage
+    localStorage.removeItem('id_token');
+    // TODO: redirect to the login page
+    
+  }
+}
+
+export default new AuthService();
